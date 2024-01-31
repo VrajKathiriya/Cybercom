39169b3f165b67aa60a3e485af5d3d0ea23069ee
vraj <vrajkathiriya2510@gmail.com>
2024-01-31 13:54:49 +0530

    productForm complete

--- JAVASCRIPT/ProductForm/script.js
+++ JAVASCRIPT/ProductForm/script.js
@@ -1,15 +1,10 @@
 let products = [];
 
 $(document).ready(function () {
-  let error = true;
-
-  (categoryError = true), (priceError = true);
-
+  let error = {};
   // product title validation
-  $("#title").focus(function () {
-    let titleError = true;
-  });
   $("#title").blur(function () {
+    error.titleError = true;
     let sameTitle = false;
     products.forEach((product) => {
       if (product.title == $("#title").val()) sameTitle = true;
@@ -19,35 +14,29 @@ $(document).ready(function () {
     } else if (sameTitle) {
       $("#titleError").text("**This title already exist");
     } else {
-      titleError = false;
+      error.titleError = false;
       $("#titleError").text("");
     }
   });
 
-  // price validation
-  $("#price").focus(function () {
-    let priceError = true;
-  });
   $("#price").blur(function () {
+    error.priceError = true;
     if (!$("#price").val()) {
       $("#priceError").text("**Please enter the price");
     } else if (!isNumber($("#price").val())) {
       $("#priceError").text("**Please enter only digits");
     } else {
-      priceError = false;
+      error.priceError = false;
       $("#priceError").text("");
     }
   });
 
-  // category validation
-  $("#title").focus(function () {
-    let categoryError = true;
-  });
   $("#category").blur(function () {
+    error.categoryError = true;
     if (!$("#category").val()) {
       $("#categoryError").text("**Please choose the category");
     } else {
-      categoryError = false;
+      error.categoryError = false;
       $("#categoryError").text("");
     }
   });
@@ -56,26 +45,66 @@ $(document).ready(function () {
   $("form").submit(function (e) {
     e.preventDefault();
 
+    // let formData = new FormData(e.target);
+    // console.log(formData);
+
     const title = $("#title").val();
     const price = $("#price").val();
     const category = $("#category").val();
 
     const newProduct = { title, price, category };
-    if (!titleError && !priceError && !categoryError) {
-      error = false;
-    }
-    if (!error) {
-      products.push(newProduct);
-      localStorage.setItem("products", JSON.stringify(products));
-      location.reload();
+
+    if (!title || !price || !category) {
+      alert("Please fill all the detail first");
+    } else if (error.titleError || error.priceError || error.categoryError) {
+      alert("Please fill detail properly");
     } else {
-      alert("please fill the appropriate detail first");
+      if ($("#submit").text() == "Add") {
+        insertEntry(newProduct);
+        location.reload();
+      } else {
+        editEntry(newProduct, $("#submit").attr("index"));
+        location.reload();
+      }
     }
   });
 });
 
 function isNumber(val) {
   return val == +val;
+}
+
+// function for entry of product
+function insertEntry(product) {
+  products = localStorage.getItem("products");
+
+  if (products == null) {
+    const temp = [];
+    localStorage.setItem("products", JSON.stringify(temp));
+  }
+  products = localStorage.getItem("products");
+  products = JSON.parse(products);
+  let index = Date.now();
+  product.index = index;
+  products.push(product);
+  localStorage.setItem("products", JSON.stringify(products));
+}
+
+// function for edit entry
+function editEntry(product, index) {
+  product.index = Number(index);
+  let products = localStorage.getItem("products");
+  products = JSON.parse(products);
+
+  let tempIndex;
+  products.forEach((p, i) => {
+    if (p.index == index) {
+      tempIndex = i;
+    }
+  });
+
+  products[tempIndex] = product;
+  localStorage.setItem("products", JSON.stringify(products));
 }
 
 products = JSON.parse(localStorage.getItem("products")) || products;
@@ -113,7 +142,11 @@ $(".edit").click(function () {
     if (p.title == id) product = p;
   });
 
+  // editEntry(product, this.index);
+
   $("#title").val(product.title);
   $("#price").val(product.price);
   $("#category").val(product.category);
+  $("#submit").text("Edit");
+  $("#submit").attr("index", product.index);
 });
