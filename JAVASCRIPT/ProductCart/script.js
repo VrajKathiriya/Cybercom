3132c9f9c41424e04fd241de786d8ed813f0edd6
vraj <vrajkathiriya2510@gmail.com>
2024-02-06 16:37:49 +0530

    product cart complete

--- JAVASCRIPT/ProductCart/script.js
+++ JAVASCRIPT/ProductCart/script.js
@@ -21,6 +21,13 @@ let productsData = [
     address: "assets/img/orange.png",
     count: 0,
   },
+  {
+    id: 4,
+    name: "Grapes",
+    price: 35.7,
+    address: "assets/img/graps.png",
+    count: 0,
+  },
 ];
 
 const productListEl = document.querySelector(".product-list");
@@ -31,27 +38,28 @@ const checkoutBtn = document.querySelector("#checkout-btn");
 document.addEventListener("DOMContentLoaded", function () {
   if (location.href.split("/")[5] == "productsList.html") {
     productsData.forEach((product) => {
-      const productNode = `<div class="product">
-      <div class="product-img">
-      <img src=${product.address} alt="" />
-      </div>
-      <div class="product-details">
-      <h3>${product.name}</h3>
-      <p>$${product.price}</p>
-</div>
-<div class="counter">
-<button id="decrement" onclick=doDecrement(${product.id})>-</button>
-  <span id=${product.id} class="count">${product.count}</span>
-  <button id="increment" onclick=doIncrement(${product.id})>+</button>
-  </div>
-  </div>`;
+      const productNode = `
+      <div class="product">
+        <div class="product-img">
+          <img src=${product.address} alt="" />
+        </div>
+        <div class="product-details">
+          <h3 class="product-name">${product.name}</h3>
+          <p class="product-price">$ ${product.price}</p>
+        </div>
+        <div class="counter">
+          <button id="decrement" onclick=doDecrement(${product.id})>-</button>
+          <span id=${product.id} class="count">${product.count}</span>
+          <button id="increment" onclick=doIncrement(${product.id})>+</button>
+        </div>
+      </div>`;
 
       productListEl.insertAdjacentHTML("beforeend", productNode);
     });
   } else {
     let ordersList = localStorage.getItem("orders");
     if (!ordersList) {
-      document.querySelector(".orders-list").innerHTML = "no order";
+      document.querySelector(".orders-list").innerHTML = "no order placed yet";
       return;
     }
     ordersList = JSON.parse(ordersList);
@@ -108,7 +116,10 @@ checkoutBtn &&
     oredersList = JSON.parse(oredersList);
 
     let newOrder = {};
-    newOrder.id = Date.now();
+
+    let date = new Date();
+    newOrder.id = `${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+
     newOrder.list = [];
 
     productsData.forEach((product) => {
@@ -120,33 +131,68 @@ checkoutBtn &&
     });
 
     newOrder.total = calculateTotalPrice();
+    newOrder.date = `${getMonth(
+      date.getMonth()
+    )} ${date.getDate()}, ${date.getFullYear()}`;
 
     oredersList.push(newOrder);
 
     localStorage.setItem("orders", JSON.stringify(oredersList));
 
+    location.href = "cart.html";
     displayOrdersList(oredersList);
   });
 
+// function which return month according to number
+function getMonth(month) {
+  if (month == 0) return "January";
+  else if (month == 1) return "February";
+  else if (month == 2) return "March";
+  else if (month == 3) return "April";
+  else if (month == 4) return "May";
+  else if (month == 5) return "June";
+  else if (month == 6) return "July";
+  else if (month == 7) return "August";
+  else if (month == 8) return "September";
+  else if (month == 9) return "October";
+  else if (month == 10) return "November";
+  else if (month == 11) return "December";
+  else return;
+}
+
+// function which display order list
 function displayOrdersList(ordersList) {
-  console.log("ij");
-  ordersList.forEach((order) => {
-    const orderNode = `<div class="order">
-    <div class="order-id">
-      <h5>Order #${order.id}</h5>
-      <p>12 february,2021</p>
-    </div>
-    <div class="order-details">
-      <span id="order-name">orange</span>
-      <span id="order-count">2 pcs</span>
-      <span id="order-price">$30</span>
-    </div>
-    <div class="order-total-price">
-      <h4>Total</h4>
-      <span id="order-total-price">$ ${order.total}</span>
-    </div>
-  </div>`;
+  ordersList.reverse().forEach((order) => {
+    const orderNode = `
+    <div class="order">
+      <div class="order-id">
+        <h5>Order #${order.id}</h5>
+        <p class="date">${order.date}</p>
+      </div>
+   
+      <div class="order-details" id="${order.id}">
+      </div>
+
+      <div class="order-total-price">
+        <h4>Total</h4>
+        <span id="order-total-price">$ ${order.total}</span>
+      </div>
+    </div>`;
 
     ordersListEl.insertAdjacentHTML("beforeend", orderNode);
+
+    const orderDetails = document.getElementById(order.id);
+
+    order.list.forEach((item) => {
+      const listNode = `
+      <div class="item-list">
+        <span class="item-box">${item.name}</span>
+        <span class="item-box">${item.count} pcs</span>
+        <span class="item-box">$ ${item.price.toFixed(2)}</span>
+      </div>`;
+
+      if (item.count == 0) return;
+      orderDetails.insertAdjacentHTML("beforeend", listNode);
+    });
   });
 }
